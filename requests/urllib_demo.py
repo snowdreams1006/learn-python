0586c23e534fbae1746e5c69504cf499d3a464a5
snowdreams1006 <snowdreams1006@163.com>
2020-01-16 07:12:12 +0800

    add use_simple_urllib2_get_headers

--- requests/urllib_demo.py
+++ requests/urllib_demo.py
@@ -2,15 +2,43 @@
 import urllib
 import urllib2
 
-URL_IP = 'http://httpbin.snowdreams1006.cn/ip'
+
 URL_GET = 'http://httpbin.snowdreams1006.cn/get'
 
-def use_simple_urllib2():
-    response = urllib2.urlopen(URL_IP)
-    print '>>>Response Headers:'
-    print response.info()
-    print '>>>Response body:'
-    print ''.join([line for line in response.readlines()])
+
+def use_simple_urllib2_get_ip():
+    '''
+    获取请求方 ip
+    '''
+    response = urllib2.urlopen('http://httpbin.snowdreams1006.cn/ip')
+    print('>>>Response Headers:')
+    print(response.info())
+    print('>>>Response Body:')
+    print(''.join([line for line in response.readlines()]))
+
+def use_simple_urllib2_get_user_agent():
+    '''
+    获取请求方 user-agent
+    '''
+    response = urllib2.urlopen('http://httpbin.snowdreams1006.cn/user-agent')
+    print('>>>Response Headers:')
+    print(response.info())
+    print('>>>Response Body:')
+    print(''.join([line for line in response.readlines()]))
+
+def use_simple_urllib2_get_headers():
+    '''
+    获取请求方 headers
+    '''
+    response = urllib2.urlopen('http://httpbin.snowdreams1006.cn/headers')
+    print('>>>Response Headers:')
+    print(response.info())
+    print('>>>Response Body:')
+    print(''.join([line for line in response.readlines()]))
+
+
+
+
 
 def use_params_urllib2():
     params = urllib.urlencode({'param1': 'hello', 'param2': 'world'})
@@ -23,8 +51,13 @@ def use_params_urllib2():
     print ''.join([line for line in response.readlines()])
 
 if __name__ == '__main__':
-    print '>>>Use simple urllib2:'
-    use_simple_urllib2()
+    print '>>>Use simple urllib2 to get ip:'
+    use_simple_urllib2_get_ip()
+    print('>>>Use simple1 urllib2 to get user-agent:')
+    use_simple_urllib2_get_user_agent()
+    print '>>>Use simple urllib2 to get headers:'
+    use_simple_urllib2_get_headers()
+
     print ''
     print '>>>Use params urllib2:'
     use_params_urllib2()
