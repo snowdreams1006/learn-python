5145aa9747bcb963d476c8a9cb9f8b179db2d1c1
snowdreams1006 <snowdreams1006@163.com>
2020-01-23 10:13:52 +0800

    get_new_url

---
+++ src/crawler/baike-python/url_manager.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+class UrlManager(object):
+    def __init__(self):
+        self.new_urls = set()
+        self.old_urls = set()
+
+    def add_new_url(self,url):
+        '''
+        添加新的 url
+        '''
+        if url is None:
+            return
+        if url not in self.new_urls and url not in self.old_urls:
+            self.new_urls.add(url)
+    
+    def add_new_urls(self,urls):
+        '''
+        批量添加新的 url
+        '''
+        if urls is None or len(urls) == 0:
+            return
+        for url in urls
+            self.add_new_url(url)
+
+    def has_new_url(self):
+        '''
+        判断是否有新的 url
+        '''
+        return len(self.new_urls) != 0
+
+    def get_new_url(self):
+        '''
+        获取新的 url
+        '''
+        new_url = self.new_urls.pop()
+        self.old_urls.add(new_url)
+        return new_url
