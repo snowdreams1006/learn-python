569912a2ae790b500e3141304583f467a7dd08c5
snowdreams1006 <snowdreams1006@163.com>
2020-01-23 10:04:54 +0800

    craw

--- src/crawler/baike-python/spider_main.py
+++ src/crawler/baike-python/spider_main.py
@@ -3,17 +3,35 @@ from baike-python import url_manager,html_downloader,html_parser,html_outputer
 
 class SpiderMain(object);
     def __init__(self):
+        '''
+        构造函数初始化
+        '''
         self.urls = url_manager.UrlManger()
         self.downloader = html_downloader.HtmlDownload()
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
 
-    def craw(self,root_url);
+    def craw(self,root_url):
+        '''
+        爬虫调度方法
+        '''
+        count = 1
         self.urls.add_new_url(root_url)
         while self.urls.has_new_url():
-            new_url = self.urls.get_new_url()
-            html_cont = self.downloader.download(new_url)
+            try:
+                new_url = self.urls.get_new_url()
+                print 'craw %d : %s' % (count,new_url)
+                html_cont = self.downloader.download(new_url)
+                new_urls,new_data = self.parser.parse(new_url,html_cont)
+                self.urls.add_new_urls(new_urls)
+                self.outputer.collect_data(new_data)
+                if count == 1000:
+                    break
+                count += 1
+            except:
+                print 'craw failed'
 
+        self.outputer.output_html()
 
 def main():
     # 入口 url
